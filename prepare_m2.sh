a32ef1a2a7124b144a848dad4f64e442c490c7fe
Chunngai <mikumikku3939@icloud.com>
2022-04-16 02:31:45 +0800

    update prepare_m2

--- prepare_m2.sh
+++ prepare_m2.sh
@@ -2,25 +2,19 @@ set -ex
 
 source ./vars.sh
 
-pip3 install -U pip setuptools wheel
+M2="$DATA/m2"
+mkdir -p $M2
 
-## ERRANT 2.0.0
-M2="$DATA/m2.w.errant.2.0.0"
-mkdir $M2
-python3.6 -m venv .errant-2-0-0
-source .errant-2-0-0/bin/activate
+python3.6 -m venv .errant
+source .errant/bin/activate
+
+pip3 install -U setuptools wheel
 pip3 install errant==2.0.0
-pip3 install "en_core_web_sm-1.2.0.tar.gz" # Downloaded from https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-1.2.0/en_core_web_sm-1.2.0.tar.gz
+if [ ! -f packages/en_core_web_sm-1.2.0.tar.gz ]; then
+  wget https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-1.2.0/en_core_web_sm-1.2.0.tar.gz -P packages/
+fi
+pip3 install packages/en_core_web_sm-1.2.0.tar.gz # Downloaded from https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-1.2.0/en_core_web_sm-1.2.0.tar.gz
 python3.6 -m spacy link en_core_web_sm en # Creates a shortcut link.
-
-# ERRANT 2.3.0
-#M2="$DATA/m2.w.errant.2.3.0"
-#mkdir $M2
-#python3.7 -m venv .errant-2-3-0
-#source .errant-2-3-0/bin/activate
-#pip3 install errant
-#pip3 install "en_core_web_sm-2.3.1.tar.gz"  # Downloaded from https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
-#python3.7 -m spacy link en_core_web_sm en # Creates a shortcut link.
 
 for dataset in "fce-train" "nucle" "lang8" "wi_locness-train" "wi_locness-valid" "fce-test" "conll14"; do
   errant_parallel -orig "$RAW/$dataset"".src" -cor "$RAW/$dataset"".trg" -out "$M2/$dataset"".m2"
