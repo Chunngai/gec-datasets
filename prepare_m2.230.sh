181331f2fd244e6992df37cd6e764da657cf1629
Chunngai <mikumikku3939@icloud.com>
2022-10-30 00:29:04 +0800

    update scripts for preparing m2 files

--- prepare_m2.230.sh
+++ prepare_m2.230.sh
@@ -16,10 +16,14 @@ fi
 pip3 install packages/"en_core_web_sm-2.3.1.tar.gz"  # Downloaded from https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
 python3.7 -m spacy link en_core_web_sm en # Creates a shortcut link.
 
-for dataset in "fce-train" "nucle" "lang8" "wi_locness-train" "wi_locness-valid" "fce-test" "conll14"; do
-  errant_parallel -orig "$RAW/$dataset"".src" -cor "$RAW/$dataset"".trg" -out "$M2/$dataset"".m2"
+for dataset in "fce-train" "nucle" "lang8" "wi_locness-train" "wi_locness-valid"; do
+  if [ ! -f "$M2/$dataset"".m2" ]; then
+    errant_parallel -orig "$RAW/$dataset"".src" -cor "$RAW/$dataset"".trg" -out "$M2/$dataset"".m2"
+  fi
 done
 
-for dataset in "train.w.err-free" "train.wo.err-free" "valid"; do
-  errant_parallel -orig "$RAW/$dataset"".src" -cor "$RAW/$dataset"".trg" -out "$M2/$dataset"".m2"
+for dataset in "valid" "train.w.err-free" "train.wo.err-free"; do
+  if [ ! -f "$M2/$dataset"".m2" ]; then
+    errant_parallel -orig "$RAW/$dataset"".src" -cor "$RAW/$dataset"".trg" -out "$M2/$dataset"".m2"
+  fi
 done
